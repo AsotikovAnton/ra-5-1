535ca3d0aaba22ab4df5877c1f92aae053ac1869
Asotikov Anton <asant@yandex.ru>
2020-04-07 19:29:01 +0300

    add task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
+import Card from './components/Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Card
+        title="Card title"
+        text="Some quick example text bla-bla-bla.">
+        <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap">
+          <rect width="100%" height="100%" fill="#868e96"></rect>
+          <text x="37%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+        </svg>
+      </Card>
+      <Card
+        title="Special title treatment"
+        text="With supporting text bla-bla-bla."
+      />
     </div>
   );
 }
